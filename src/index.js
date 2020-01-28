8b00e0b90331ff62ac8038d65efce27238e3b310
aboudicheng <aboudicheng@gmail.com>
2020-01-29 00:07:20 +0800

    Remove unused dependency

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useCallback } from "react";
+import { useState, useEffect, useReducer, useCallback } from "react";
 
 function useOptimisticReducer() {
   const [scheduler, setScheduler] = useState({});
